a2ad82ddce7457cb1924c6e9cd9872c94be5a502
Dustin Dettmer <dustinpaystaxes@gmail.com>
2013-03-05 11:10:47 -0800

    Initial Commit

---
+++ Golf/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Golf
+//
+//  Created by Dustin Dettmer on 3/5/13.
+//  Copyright (c) 2013 Dustin Dettmer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
